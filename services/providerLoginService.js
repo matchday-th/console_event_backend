b09eacb9bbbec8f786a503eab4e3efab4850a699
hutn <tunnaing@gmail.com>
2026-04-10 18:01:29 +0700

    save

--- services/providerLoginService.js
+++ services/providerLoginService.js
@@ -3,6 +3,9 @@ const Providers = require("../models/providers");
 async function getProviders({ page, perPage, search }) {
     let query = Providers.query()
             .select('id', 'fullname', 'url_nickname', 'phone_number')
+            .where(function() {
+                    this.where('removed', 0).orWhereNull('removed');
+                })
 
     if (search) {
         query = query.where('fullname', 'like', `%${search}%`);
